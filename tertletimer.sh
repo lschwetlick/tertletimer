a1fdfa9012a5457fe8555d5b144b535cc554083c
Lisa Schwetlick <mail@lisaschwetlick.de>
2020-05-24 18:27:17 +0200

    added absolute date support

--- tertletimer.sh
+++ tertletimer.sh
@@ -8,19 +8,22 @@
 #-h hours
 #-m minutes
 #-s seconds
+#-p print style
+#-a absolute time at which to alert. Format: "2020-05-25 11:30:00". needs gnudate.
 
 DURATION_HR=0
 DURATION_MIN=0
 DURATION_SEC=0
-PRINT=1
-ABSOLUTE=false
+PRINT=2
+ABSOLUTE=""
 
 # Get hours minutes and seconds as input
 OPTIND=1
-while getopts "h:m:s:p:a" opt; do
+while getopts "h:m:s:p:a:" opt; do
     case "$opt" in
-    h)
-	DURATION_HR=$OPTARG
+    a)  ABSOLUTE=$OPTARG
+	    ;;
+    h)  DURATION_HR=$OPTARG
         ;;
     m)  DURATION_MIN=$OPTARG
         ;;
@@ -28,14 +31,11 @@ while getopts "h:m:s:p:a" opt; do
         ;;
     p)  PRINT=$OPTARG
 	    ;;
-    a)  ABSOLUTE=$OPTARG
-	    ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
         ;;
     esac
 done
-
 # Font Setup
 case $PRINT in
    1) oneline=true;;
@@ -56,9 +56,26 @@ function finish {
 }
 trap finish EXIT
 
-
 # Get total duration of timer in seconds
-DURATION=$(( $DURATION_HR *3600 + $DURATION_MIN *60 + $DURATION_SEC))
+if ! [ "$ABSOLUTE" = "" ]
+then
+
+    if hash gdate 2>/dev/null; then
+        DEADLINE=$( gdate -d "$ABSOLUTE" +%s)
+    else
+        DEADLINE=$( date -d "$ABSOLUTE" +%s)
+    fi
+
+    NOW=$(date +%s)
+    DURATION=$(( $DEADLINE - $NOW ))
+    if [ $DURATION -lt 0 ]
+    then
+        echo "The date you passed is in the past!"
+        exit 0
+    fi
+else
+    DURATION=$(( $DURATION_HR *3600 + $DURATION_MIN *60 + $DURATION_SEC))
+fi
 echo $DURATION
 # hide cursor for the duration of the timer
 tput civis
@@ -106,7 +123,7 @@ while [ -1 ]; do
 	then 					# blink screen
 		if $oneline
 		then
-			printf "\r\e%0.2d:%0.2d:%0.2d" $HR $MINS $SECS
+			printf "\r\e %0.2d:%0.2d:%0.2d" $HR $MINS $SECS
 		else
 			printf "%0.2d:%0.2d:%0.2d" $HR $MINS $SECS | figlet -f $font
 		fi
@@ -132,7 +149,7 @@ while [ -1 ]; do
 		#printf "\r\e %0.2d:%0.2d:%0.2d" $HR $MINS $SECS
 		if $oneline
 		then
-			printf "\r\e%0.2d:%0.2d:%0.2d" $HR $MINS $SECS
+			printf "\r\e %0.2d:%0.2d:%0.2d" $HR $MINS $SECS
 		else
 			printf "%0.2d:%0.2d:%0.2d" $HR $MINS $SECS | figlet -f $font
 		fi
